041f7dc8d7b6dc9bbee8dd0050f6c102fba8f60d
Mennaahmed08 <menna128492@gmail.com>
2026-02-03 23:22:50 +0200

    Update Home, add Category, improve README, add Support & T&C, update meta description for SEO/share

--- Thoutha-Website/src/Components/Footer.jsx
+++ Thoutha-Website/src/Components/Footer.jsx
@@ -1,19 +1,13 @@
-// import { Link, NavLink } from "react-router-dom";
+import { Link} from "react-router-dom";
 import "../Css/Footer.css";
 
 export default function Footer() {
   return (
     <footer className="footer-container">
-      {/* <div className="footer-links">
-        <NavLink to="/" className={({ isActive }) => (isActive ? "footer-active-link" : "")}>الصفحه الرئيسية</NavLink>
-        <NavLink to="/profile" className={({ isActive }) => (isActive ? "footer-active-link" : "")}>الملف الشخصي</NavLink>
-        <NavLink to="/chatbot" className={({ isActive }) => (isActive ? "footer-active-link" : "")}>الطبيب الذكي</NavLink>
-        <NavLink to="/terms&conditions" className={({ isActive }) => (isActive ? "footer-active-link" : "")}>الشروط والاستخدام</NavLink>
-        <NavLink to="/login" className={({ isActive }) => (isActive ? "footer-active-link" : "")}>تسجيل دخول / انشاء حساب</NavLink>
-      </div> */}
       <div className="footer-copy">
         &copy; 2026 ثوثة. جميع الحقوق محفوظة.
       </div>
+      <Link to="/support" className="footer-copy">الدعم الفنى</Link>
     </footer>
   );
 }
